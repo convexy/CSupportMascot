f0fa0d7c195b571deebbef4b6109d5d1d7626d14
shibuya.keigo <shibuya.keigo@AMUY-2105>
2025-05-17 21:18:35 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CSupportMascot
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
